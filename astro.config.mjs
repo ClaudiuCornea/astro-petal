051b098e281231c11529f4835cef31529723d41d
Claudiu <claudiu.simion.cornea@gmail.com>
2025-04-20 13:20:53 +0200

    added alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import solidJs from '@astrojs/solid-js';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), alpinejs()],
 
   vite: {
     plugins: [tailwindcss()]
