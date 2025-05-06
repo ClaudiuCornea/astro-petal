320d492cc2f3de4de0a18269ac230caa0ad307a5
Claudiu <claudiu.simion.cornea@gmail.com>
2025-05-06 23:30:51 +0200

    Css framework

---
+++ menu/menu.js
@@ -0,0 +1,258 @@
+export const menu = {
+	'soups': [
+		{
+			'name': 'Phở Đặc Biệt',
+			'price': {
+				'small': 13.00,
+				'normal': 0,
+				'large': 14.00,
+			},
+			'description': 'Rice noodle soup with beef',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Bún Bò Huế',
+			'price': {
+				'small': 13.00,
+				'normal': 0,
+				'large': 14.00,
+			},
+			'description': 'Rice noodle soup with beef style Huế',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Bún Riêu',
+			'price': {
+				'small': 13.00,
+				'normal': 0,
+				'large': 14.00,
+			},
+			'description': 'Crab soup with vermicelli',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Hủ Tiếu Nam Vang',
+			'price': {
+				'small': 13.00,
+				'normal': 0,
+				'large': 14.00,
+			},
+			'description': 'Rice noodle soup with shrimp and pork',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Tom Kha Kai',
+			'price': {
+				'small': 0,
+				'normal': 7.50,
+				'large': 0,
+			},
+			'description': 'Chicken soup with or without coconut milk',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Tom Kha Kung',
+			'price': {
+				'small': 0,
+				'normal': 8.50,
+				'large': 0,
+			},
+			'description': 'Shrimp soup with or without coconut milk',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Hoằn Thắn',
+			'price': {
+				'small': 0,
+				'normal': 7.50,
+				'large': 0,
+			},
+			'description': 'Wan Tan soup with pork',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Mì Vịt',
+			'price': {
+				'small': 0,
+				'normal': 8.50,
+				'large': 0,
+			},
+			'description': 'Wan Tan soup with shrimps',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Canh Cay',
+			'price': {
+				'small': 0,
+				'normal': 5.50,
+				'large': 0,
+			},
+			'description': 'Hot and sour soup with chicken',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Canh Yến',
+			'price': {
+				'small': 0,
+				'normal': 5.50,
+				'large': 0,
+			},
+			'description': 'Bird nest soup',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Canh Miến',
+			'price': {
+				'small': 0,
+				'normal': 5.50,
+				'large': 0,
+			},
+			'description': 'Bird nest soup with chicken',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Mì Vịt',
+			'price': {
+				'small': 0,
+				'normal': 15.00,
+				'large': 0,
+			},
+			'description': 'Noodle soup with peking duck',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Mì Sủi Kảo',
+			'price': {
+				'small': 0,
+				'normal': 15.00,
+				'large': 0,
+			},
+			'description': 'Noodle soup with shrimps',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Canh Măng Cua',
+			'price': {
+				'small': 0,
+				'normal': 5.50,
+				'large': 0,
+			},
+			'description': 'Asparagus soup with crab',
+			'origin': 'vientnam',
+		},
+	],
+	'appetizers': [
+		{
+			'name': 'Chả Giò Việt Nam',
+			'price': {
+				'small': 0,
+				'normal': 7.20,
+				'large': 0,
+			},
+			'description': 'Fried spring roll with shrimp and chicken',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Càng Cua',
+			'price': {
+				'small': 0,
+				'normal': 6.50,
+				'large': 0,
+			},
+			'description': 'Fried crab clam wrapped with shrimps',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Càng Cua',
+			'price': {
+				'small': 0,
+				'normal': 6.50,
+				'large': 0,
+			},
+			'description': 'Fried crab clam wrapped with shrimps',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Chao Tôm',
+			'price': {
+				'small': 0,
+				'normal': 6.50,
+				'large': 0,
+			},
+			'description': 'Sugarcane wrapped with shrimp',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Bánh cuốn chả lụa',
+			'price': {
+				'small': 0,
+				'normal': 10.50,
+				'large': 0,
+			},
+			'description': 'Steamed cake roll',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Ốc nướng',
+			'price': {
+				'small': 0,
+				'normal': 13.00,
+				'large': 0,
+			},
+			'description': 'Grilled snails',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Gỏi Cuốn Tôm',
+			'price': {
+				'small': 0,
+				'normal': 8.00,
+				'large': 0,
+			},
+			'description': 'Spring roll with shrimp',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Tôm Chiên Bột',
+			'price': {
+				'small': 0,
+				'normal': 9.00,
+				'large': 0,
+			},
+			'description': 'Battered shrimp',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Bò Lá Lốt',
+			'price': {
+				'small': 0,
+				'normal': 16.00,
+				'large': 0,
+			},
+			'description': 'Grilled Lop leave beef with vermicelli',
+			'origin': 'vientnam',
+		},
+		{
+			'name': 'Ếch chiên bơ tỏi',
+			'price': {
+				'small': 0,
+				'normal': 12.00,
+				'large': 0,
+			},
+			'description': 'Frozen frog legs with garlic',
+			'origin': 'vientnam',
+		},
+	],
+	'noodles-and-rice': [
+		{
+			'name': 'Fried rice noodle with vegetables',
+			'price': {
+				'small': 0,
+				'normal': 13.00,
+				'large': 0,
+			},
+			'description': 'Fried rice noodle with vegetables',
+			'origin': 'vientnam',
+		},
+	]
+}
